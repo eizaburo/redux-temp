9d0e754c581e360e4fb0a3715a1aa3652b8782c7
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2018-11-06 07:29:49 +0900

    ファイルを用意Homeを表示

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './screens/Home';
+import Detail from './screens/Detail';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
